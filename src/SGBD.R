13f34007db72648c17b3f1c8afc22c079a8140d1
Pramodh-G <pramodh@iitk.ac.in>
2022-02-08 09:53:03 +0530

    Add options for SGBD-Bimodal

--- src/SGBD.R
+++ src/SGBD.R
@@ -67,23 +67,53 @@ barker <- function(y, X, N = 1e3, prop.sd = 0.35)
     return(beta.mat)
 }
 
+barker_bim <- function(y, X, N = 1e3, prop.sd = 0.35)
+{
+    p <- dim(X)[2]
+
+    foo <- glm(y ~ X - 1, family = binomial("logit"))$coef
+    beta <- as.matrix(foo, ncol = 1)
+    beta.mat <- matrix(0, nrow = N, ncol = p)
+    beta.mat[1, ] <- as.numeric(beta)
+
+
+    for (i in 2:N)
+    {
+        z <- rnorm(p, mean = (sqrt(1 - prop.sd^2)), sd = prop.sd)
+        if(runif(1) < 0.5)
+        {
+            z <- -z
+        }
+        prob_invert <- 1 / (1 + exp(-z * grad_log_posterior(beta)))
+        inv_or_not <- (runif(p) < prob_invert)
+        b <- 2 * inv_or_not - 1
+        beta.mat[i, ] <- beta + z * b
+    }
+
+    return(beta.mat)
+}
+
 chain <- barker(y, X, N = 1e3, prop.sd = 4.5e-3)
 sigma_est <- diag(cov(chain))
 chain <- barker(y, X, N = 1e5, prop.sd = 4 * sigma_est)
 # nasty surprises on increasing N. Maybe needs more samples to explore space. All of a sudden drop in acceptance.
 
+chain_bim <- barker_bim(y, X, N = 1e3, prop.sd = 4.5e-3)
+sigma_est_bim <- diag(cov(chain))
+chain_bim <- barker(y, X, N = 1e5, prop.sd = 4 * sigma_est)
+
 
 # diagnostics.
-plot.ts(chain)
+plot.ts(chain_bim)
 
 par(mfrow = c(2, 3))
 
 for (i in 1:dim(chain)[2])
 {
-    acf(chain[, i], main = paste("ACF for component ", i))
+    acf(chain_bim[, i], main = paste("ACF for component ", i))
 }
 
 for (i in 1:dim(chain)[2])
 {
-    plot(density(chain[, i]), main = paste("density for component ", i))
+    plot(density(chain_bim[, i]), main = paste("density for component ", i))
 }
